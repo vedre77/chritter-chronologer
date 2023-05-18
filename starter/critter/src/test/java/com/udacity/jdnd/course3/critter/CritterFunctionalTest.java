18b63d9744a71c1aa7a91ff4d7980e2f37a28a42
vedre77 <vedranvitas@net.hr>
2023-05-18 20:11:22 +0200

    Imports edit.

--- starter/critter/src/test/java/com/udacity/jdnd/course3/critter/CritterFunctionalTest.java
+++ starter/critter/src/test/java/com/udacity/jdnd/course3/critter/CritterFunctionalTest.java
@@ -8,6 +8,7 @@ import com.udacity.jdnd.course3.critter.pet.PetType;
 import com.udacity.jdnd.course3.critter.schedule.ScheduleController;
 import com.udacity.jdnd.course3.critter.schedule.ScheduleDTO;
 import com.udacity.jdnd.course3.critter.user.*;
+import com.udacity.jdnd.course3.critter.user.customer.CustomerDTO;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
