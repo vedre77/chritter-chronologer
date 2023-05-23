1bca67472869c560d5d3bf059f4c4bec86c97e7a
vedre77 <vedranvitas@net.hr>
2023-05-23 10:27:08 +0200

    DTO methods moved from controller, added get all cust., modif. save cust.

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/customer/CustomerService.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/customer/CustomerService.java
@@ -1,9 +1,14 @@
 package com.udacity.jdnd.course3.critter.user.customer;
 
+import com.udacity.jdnd.course3.critter.user.BaseUser;
 import com.udacity.jdnd.course3.critter.user.BaseUserRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
@@ -12,8 +17,31 @@ public class CustomerService {
     private BaseUserRepository baseUserRepository;
 
     @Transactional
-    public Customer saveCustomer(Customer customer) {
-        baseUserRepository.save(customer);
+    public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
+        Customer newCustomer = convertCustomerDTOToCustomer(customerDTO);
+        baseUserRepository.save(newCustomer);
+        return customerDTO;
+    }
+
+    public List<CustomerDTO> getAllCustomers() {
+        List<BaseUser> baseUsers = baseUserRepository.findAll();
+        return baseUsers.stream()
+                .filter(user -> user instanceof Customer)
+                .map(user -> (Customer) user)
+                .map(this::convertCustomerToCustomerDTO)
+                .collect(Collectors.toList());
+    }
+    private CustomerDTO convertCustomerToCustomerDTO(Customer customer) {
+        CustomerDTO customerDTO = new CustomerDTO();
+        BeanUtils.copyProperties(customer, customerDTO);
+        return customerDTO;
+    }
+
+    private Customer convertCustomerDTOToCustomer(CustomerDTO customerDTO) {
+        Customer customer = new Customer();
+        customer.setName(customerDTO.getName());
+        customer.setPhoneNumber(customerDTO.getPhoneNumber());
+        //customer.setPets(new HashSet<>());
         return customer;
     }
 }
