e8daee352c6623fd820ff2993902449f8b19350e
vedre77 <vedranvitas@net.hr>
2023-05-25 17:13:04 +0200

    Added days available field.

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/employee/Employee.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/employee/Employee.java
@@ -2,6 +2,8 @@ package com.udacity.jdnd.course3.critter.user.employee;
 
 import com.udacity.jdnd.course3.critter.user.BaseUser;
 import javax.persistence.*;
+import java.time.DayOfWeek;
+import java.util.Set;
 
 @Entity
 @DiscriminatorValue("employee")
@@ -9,8 +11,27 @@ public class Employee extends BaseUser {
     @Column(name = "title")
     private String title;
 
+    @ElementCollection(targetClass = DayOfWeek.class)
+    private Set<DayOfWeek> daysAvailable;
+
     //@ManyToMany(mappedBy = "employees")
     //private List<Schedule> schedules;
 
     // getters and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public Set<DayOfWeek> getDaysAvailable() {
+        return daysAvailable;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDaysAvailable(Set<DayOfWeek> daysAvailable) {
+        this.daysAvailable = daysAvailable;
+    }
 }
