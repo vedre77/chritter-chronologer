114a58589f8cceefce33cf337bd4c2e4fa143dca
vedre77 <vedranvitas@net.hr>
2023-05-26 15:20:05 +0200

    Removed comments.

--- starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/customer/CustomerRepository.java
+++ starter/critter/src/main/java/com/udacity/jdnd/course3/critter/user/customer/CustomerRepository.java
@@ -7,12 +7,6 @@ import java.util.Optional;
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    // derived query (owner has pets, a collection of pet entities)
     Optional<Customer> findByPetsId(Long petId);
 
-     /**
-     custom query would look like:
-     @Query("SELECT c FROM Customer c JOIN c.pets p WHERE p.id = :petId")
-     Optional<Customer> findOwnerByPetId(@Param("petId") Long petId);
-     **/
 }
